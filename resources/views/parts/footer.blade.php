e1d74820e420688994b43c33d491b76a2cbd84bd
Edu Aymerich <eduardaymerich@gmail.com>
2019-02-01 01:46:00 +0100

    update footer with github link

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,12 +1,19 @@
+<!-- Footer -->
 <footer class="page-footer">
+
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">
         <ul class="list-inline">
-            <li class="list-inline-item"> © 2019 - <?php echo date("Y"); ?> chesst </li>
+            <li class="list-inline-item"> © <?php echo date("Y"); ?> chesst </li>
             <li class="list-inline-item"> | </li>
-            <li class="list-inline-item"> All rights reserved </li>
+            <li class="list-inline-item">
+              <a href="https://github.com/eduayme/chesst" target="_blank">
+                Free & Open Source
+              </a>
+            </li>
             <li class="list-inline-item"> | </li>
             <li class="list-inline-item"> Version 1.0 </li>
         </ul>
     </div>
+
 </footer>
