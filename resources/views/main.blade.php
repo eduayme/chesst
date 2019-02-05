c8d1de3b12611dcce2fd210b2a3344c04a9682cc
Edu Aymerich <eduardaymerich@gmail.com>
2019-02-05 19:30:10 +0100

    add alerts to main view

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,19 @@
 
 @section('content')
 
+    <!-- Alerts -->
+    @if( session()->get('success') )
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            {{ session()->get('success') }}
+        </div><br/>
+    @elseif( session()->get('error') )
+        <div class="alert alert-error alert-dismissible fade show" role="alert">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            {{ session()->get('error') }}
+        </div><br/>
+    @endif
+
     <div class="text-center">
         <h1 class="card-title"> Welcome to ChessT </h1>
         <p class="card-text"> The best place to find all chess tournaments </p>
