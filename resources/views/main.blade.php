6446bc81617b498f42969a54841be1ac66ffa6dc
Edu Aymerich <eduardaymerich@gmail.com>
2019-02-07 23:24:23 +0100

    added octicons icons and improved landing page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,6 +17,7 @@
         </div><br/>
     @endif
 
+    <!-- Header -->
     <div class="text-center" style="margin: 100px">
         <h1 class="card-title"> Welcome to ChessT </h1>
         <p class="card-text"> The best place to find chess tournaments </p>
@@ -28,6 +29,24 @@
            class="btn btn-outline-secondary btn-lg" role="button">
            Create Tournament
         </a>
+    </div>
+
+    <!-- Advantages -->
+    <div class="container" style="margin: 50px 0">
+      <div class="row">
+        <div class="col-sm-4 text-center">
+            <span class="octicon octicon-search" style="font-size: 100px;"></span>
+            <h3> Find quick and easy a lot of chess tournaments around the world </h3>
+        </div>
+        <div class="col-sm-4 text-center">
+          <span class="octicon octicon-cloud-upload" style="font-size: 100px;"></span>
+          <h3> Upload your own tournaments to reach a lot of chess players </h3>
+        </div>
+        <div class="col-sm-4 text-center">
+          <span class="octicon octicon-database" style="font-size: 100px;"></span>
+          <h3> One of the biggest chess tournaments database on internet </h3>
+        </div>
+      </div>
     </div>
 
 @endsection
--- resources/views/parts/nav.blade.php
+++ resources/views/parts/nav.blade.php
@@ -59,6 +59,7 @@
 
                         <a class="dropdown-item" href="{{ route('logout') }}"
                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                           <span class="octicon octicon-sign-out" style="font-size: 15px;"></span>
                             {{ __('Logout') }}
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
