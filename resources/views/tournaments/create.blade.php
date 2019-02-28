0a15c070c05a324718375903aa63016754ddc681
Edu Aymerich <eduardaymerich@gmail.com>
2019-02-28 02:35:55 +0100

    automatic country and city detection from address input

--- resources/views/tournaments/create.blade.php
+++ resources/views/tournaments/create.blade.php
@@ -8,6 +8,9 @@
 <!-- Mapbox geocoder -->
 <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v3.1.2/mapbox-gl-geocoder.min.js'></script>
 <link href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v3.1.2/mapbox-gl-geocoder.css' rel='stylesheet' type='text/css' />
+<!-- Geocoder for city and country -->
+<script src="http://js.api.here.com/v3/3.0/mapsjs-core.js" type="text/javascript" charset="utf-8"></script>
+<script src="http://js.api.here.com/v3/3.0/mapsjs-service.js" type="text/javascript" charset="utf-8"></script>
 
 @section('content')
 
@@ -94,21 +97,21 @@
                     </div>
                 </div>
 
+                <?php /*
                 <!-- County & City -->
                 <div class="form-row">
                     <!-- Country -->
                     <div class="form-group col-md-6">
                         <label for="begin"> {{ __('tournaments.country') }}: </label>
-                        <select class="form-control" name="country">
-                            @include('parts.selectCountries')
-                        </select>
+                        <input id="country" type="text" class="form-control" name="country"/>
                     </div>
                     <!-- City -->
                     <div class="form-group  col-md-6">
                         <label for="begin"> {{ __('tournaments.city') }}: </label>
-                        <input type="text" class="form-control" name="city"/>
+                        <input id="city" type="text" class="form-control" name="city"/>
                     </div>
                 </div>
+                */ ?>
 
                 <!-- Address -->
                 <style> .mapboxgl-ctrl-geocoder { min-width:100%; } </style>
@@ -123,16 +126,22 @@
                 <div id='map' style='width: 100%; height: 300px;'></div>
 
                 <!-- Address HIDDEN -->
-                <input type="hidden" name="address"/>
+                <input id="address" type="hidden" name="address"/>
+
+                <!-- Country HIDDEN -->
+                <input id="country" type="hidden" name="country"/>
+
+                <!-- Cityy HIDDEN -->
+                <input id="city" type="hidden" name="city"/>
 
                 <!-- Latitude HIDDEN -->
-                <input type="hidden" name="latitude"/>
+                <input id="lat" type="hidden" name="latitude"/>
 
                 <!-- Longitude HIDDEN -->
-                <input type="hidden" name="longitude"/>
+                <input id="lng" type="hidden" name="longitude"/>
 
                 <!-- User_id HIDDEN -->
-                <input type="hidden" name="user_id" value={{ Auth::user()->id }}/>
+                <input type="hidden" name="user_id" value={{ Auth::user()->id }}>
 
                 <!-- Submit button -->
                 <div class="text-center" style="margin-top: 15px">
@@ -248,13 +257,50 @@
         // makes a selection and add a symbol that matches the result.
         geocoder.on('result', function(ev) {
           map.getSource('single-point').setData(ev.result.geometry);
+
+          // begin address
+          console.log( '----------------------------------------------' );
+
+          // get address
+          console.log( 'Address: ' + ev.result.place_name );
+          document.getElementById("address").value = ev.result.place_name;
+
           // get latitude
           var latitude = ev.result.geometry.coordinates[1];
           console.log( 'Latitude: ' + latitude );
+          document.getElementById("lat").value = latitude;
+
           // get longitude
           var longitude = ev.result.geometry.coordinates[0];
           console.log( 'Longitude: ' + longitude );
+          document.getElementById("lng").value = longitude;
+
+          // get city
+          var city = '';
+          var length = ev.result.context.length;
+          if( length > 0 && length < 3 ) {
+              city = ev.result.text;
+          }
+          else if( length < 4 && length > 2  ) {
+              city = ev.result.context[0].text;
+          }
+          else if( length == 4 ) {
+              city = ev.result.context[1].text;
+          }
+          else if( length == 5 ) {
+              city = ev.result.context[2].text;
+          }
+          console.log( 'City: ' + city );
+          document.getElementById("city").value = city;
+
+          // get country
+          var country = ev.result.context[ev.result.context.length-1].text;
+          console.log( 'Country: ' + country );
+          document.getElementById("country").value = country;
           });
+
+          // end address
+          console.log( '----------------------------------------------' );
         });
 
     });
