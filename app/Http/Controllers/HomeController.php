e26d332894d9398502f64bda320bc2791dc235f0
Edu Aymerich <eduardaymerich@gmail.com>
2019-08-08 18:24:59 +0200

    added next tournaments in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,13 @@
 
 namespace ChessT\Http\Controllers;
 
+use Auth;
+use Carbon\Carbon;
+use ChessT\Tournament;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
@@ -23,7 +17,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('main');
+        $tournaments = Tournament::all()->where( 'end', '>=', Carbon::today() )->take(10);
+
+        $categories = Tournament::select( 'category' )->groupBy( 'category' )->take(5)->get();
+
+        $countries = Tournament::select( 'country' )->groupBy( 'country' )->take(5)->get();
+
+        $cities = Tournament::select( 'city' )->groupBy( 'city' )->take(5)->get();
+
+        return view( 'main',
+        compact( 'tournaments', 'categories', 'countries', 'cities' ) );
     }
 
 }
